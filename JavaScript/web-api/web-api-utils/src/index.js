10c09b6e2ab74e059fad87fc8453cd30fdcbd33d
pad1ryoshi <rodripadidev@gmail.com>
2025-02-02 17:05:15 -0300

    Resposta do servidor corrigida.

--- JavaScript/web-api/web-api-utils/src/index.js
+++ JavaScript/web-api/web-api-utils/src/index.js
@@ -8,22 +8,40 @@ app.use(express.json());
 
 app.post('/util/text/lowercase', (req, res) => {
   const input = req.body.input;
-  res.send({ output: toLowerCase(input) });
+  res.send({
+    action: 'lowercase',
+    input: input,
+    output: toLowerCase(input)
+  });
 });
 
 app.post('/util/text/uppercase', (req, res) => {
   const input = req.body.input;
-  res.send({ output: toUpperCase(input) });
+  res.send({
+    action: 'uppercase',
+    input: input,
+    output: toUpperCase(input)
+  });
 });
 
 app.get('/util/number/minimum', (req, res) => {
-  const inputs = req.query.input.split(',').map(Number);
-  res.send({ output: findMinimum(inputs) });
+  const inputs = req.query.input.split(',');
+  const numericInputs = inputs.map(Number);
+  res.send({
+    action: 'minimum',
+    input: inputs,
+    output: findMinimum(numericInputs).toString()
+  });
 });
 
 app.get('/util/number/maximum', (req, res) => {
-  const inputs = req.query.input.split(',').map(Number);
-  res.send({ output: findMaximum(inputs) });
+  const inputs = req.query.input.split(',');
+  const numericInputs = inputs.map(Number);
+  res.send({
+    action: 'maximum',
+    input: inputs,
+    output: findMaximum(numericInputs).toString()
+  });
 });
 
 app.listen(port, () => {
